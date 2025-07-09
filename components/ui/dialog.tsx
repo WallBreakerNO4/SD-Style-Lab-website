a017b5c239c295890822076a37c4027c3497be3e
WallBreakerNO4 <zhifanguo@gmail.com>
2025-07-09 21:24:01 +0800

    fix(主页): 继续修改让查看大图时的边框不显示

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -64,6 +64,7 @@ function DialogContent({
         className={cn(
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] duration-200 sm:max-w-lg",
           !unpadded && "gap-4 rounded-lg border bg-background p-6 shadow-lg",
+          unpadded && "focus:outline-none",
           className
         )}
         {...props}
