e38ae9b3b7af9d58e76de5c6e4ac2f9ad128745e
WallBraekerNO4 <zhifanguo@gmail.com>
2025-08-14 09:51:48 +0800

    feat: 添加Vercel Analytics分析功能并优化代码格式

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default async function Home() {
         <main>
           {models.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 md:gap-6">
-              {models.map((model, index) => (
+              {models.map((model) => (
                 <div key={model.model_name}>
                   <ModelCard model={model} />
                 </div>
